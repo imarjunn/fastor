bb513ae8c5430a4118b85b8170b90ce85f08cee1
imarjunn <singh.arjun535@gmail.com>
2020-12-09 21:28:08 +0530

    HTML5History Route issue solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route,NavLink } from "react-router-dom";
+import { HashRouter as Router, Switch, Route,NavLink } from "react-router-dom";
 
 import Register from './components/Register';
 import Login from './components/Login';
@@ -8,7 +8,7 @@ import Restaurents from './components/Restaurents';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/">
       <Switch>
         <Route exact path="/otp-verify" component={Login} />  
         <Route exact path="/rest-listing" component={Restaurents} /> 
